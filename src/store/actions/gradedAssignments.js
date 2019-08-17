b068c89b839d0d4024d2eed8b20364b597f8ba90
olehedza <olehedza@gmail.com>
2019-08-17 20:15:00 +0300

    finish store actions & create all reducers

--- src/store/actions/gradedAssignments.js
+++ src/store/actions/gradedAssignments.js
@@ -1,23 +1,60 @@
-export const AUTH_START = "AUTH_START";
-export const AUTH_SUCCESS = "AUTH_SUCCESS";
-export const AUTH_FAIL = "AUTH_FAIL";
-export const AUTH_LOGOUT = "AUTH_LOGOUT";
+import axios from "axios";
+import * as actionTypes from "./actionTypes";
 
-export const GET_ASSIGNMENT_LIST_START = "GET_ASSIGNMENT_LIST_START";
-export const GET_ASSIGNMENTS_LIST_FAIL = "GET_ASSIGNMENTS_LIST_FAIL";
-export const GET_ASSIGNMENTS_LIST_SUCCESS = "GET_ASSIGNMENTS_LIST_SUCCESS";
+const getGradedASNTListStart = () => {
+  return {
+    type: actionTypes.GET_GRADED_ASSIGNMENT_LIST_START
+  };
+};
 
-export const GET_ASSIGNMENT_DETAIL_START = "GET_ASSIGNMENT_DETAIL_START";
-export const GET_ASSIGNMENT_DETAIL_FAIL = "GET_ASSIGNMENT_DETAIL_FAIL";
-export const GET_ASSIGNMENT_DETAIL_SUCCESS = "GET_ASSIGNMENT_DETAIL_SUCCESS";
+const getGradedASNTListSuccess = assignments => {
+  return {
+    type: actionTypes.GET_GRADED_ASSIGNMENTS_LIST_SUCCESS,
+    assignments
+  };
+};
 
-export const CREATE_ASSIGNMENT_START = "CREATE_ASSIGNMENT_START";
-export const CREATE_ASSIGNMENT_FAIL = "CREATE_ASSIGNMENT_FAIL";
-export const CREATE_ASSIGNMENT_SUCCESS = "CREATE_ASSIGNMENT_SUCCESS";
+const getGradedASNTListFail = error => {
+  return {
+    type: actionTypes.GET_GRADED_ASSIGNMENTS_LIST_FAIL,
+    error: error
+  };
+};
 
-export const GET_GRADED_ASSIGNMENT_LIST_START =
-  "GET_GRADED_ASSIGNMENT_LIST_START";
-export const GET_GRADED_ASSIGNMENTS_LIST_FAIL =
-  "GET_GRADED_ASSIGNMENTS_LIST_FAIL";
-export const GET_GRADED_ASSIGNMENTS_LIST_SUCCESS =
-  "GET_GRADED_ASSIGNMENTS_LIST_SUCCESS";
+export const getGradedASNTS = (username, token) => {
+  return dispatch => {
+    dispatch(getGradedASNTListStart());
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`
+    };
+    axios
+      .get(`http://127.0.0.1:8000/graded-assignments/?username=${username}`)
+      .then(res => {
+        const assignments = res.data;
+        dispatch(getGradedASNTListSuccess(assignments));
+      })
+      .catch(err => {
+        dispatch(getGradedASNTListFail(err));
+      });
+  };
+};
+
+export const createGradedASNT = (token, asnt) => {
+  return dispatch => {
+    //   dispatch(createASNTStart());
+    axios.defaults.headers = {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`
+    };
+    axios
+      .post(`http://127.0.0.1:8000/graded-assignments/create/`, asnt)
+      .then(res => {
+        console.log("success");
+        //   dispatch(createASNTSuccess());
+      })
+      .catch(err => {
+        //   dispatch(createASNTFail());
+      });
+  };
+};
